40d636253495df163b78e62f15df4878117ab962
Lillian Austin <lillian542@gmail.com>
2019-03-16 00:21:04 +0100

    Add option to run optimization on completed dataset

--- qdev_wrappers/optimization/Methods.py
+++ qdev_wrappers/optimization/Methods.py
@@ -1,7 +1,7 @@
 import qcodes as qc
 from numpy.random import randint
 from itertools import product
-from qdev_wrappers.optimization.readout_fidelity_alazar import calculate_fidelity
+from qdev_wrappers.automated_tuneup.readout_fidelity_alazar import calculate_fidelity
 from qdev_wrappers.fitting.fitter import Fitter
 import qdev_wrappers.fitting.least_squares_models as lsm
 import numpy as np
@@ -9,15 +9,17 @@ import numpy as np
 
 class ReadoutFidelityOptimization:
 
-    def __init__(self, *params, get_data, max_attempts=250):
+    def __init__(self, *params, get_data, max_attempts=20, runid=None, from_saved_data=False):
         # get_data: pwa.alazar_channels.data
         # ToDo: figure out better solution for step size?
         # ToDo: I think params just go in the optimize function, instead of in the model, so model just needs get_data
-        self.params = [item for item in params if isinstance(item, qc.Parameter)]
+        self.params = [item for item in params if isinstance(item, qc.Parameter) or isinstance(item, str)]
         self.step_size = [item for item in params if isinstance(item, float)]
         self.measured_params = {'readout_fidelity': {'label': 'Readout Fidelity', 'unit': ''}}
         self.max_attempts = max_attempts
         self.get_data = get_data
+        self.from_saved_data = from_saved_data
+        self.runid = runid
 
     def check_next(self, current_location):
         """Takes current location in parameter space, decides next coordinates to measure
@@ -33,17 +35,21 @@ class ReadoutFidelityOptimization:
         pass
         # return next_location
 
-    def measurement_function(self):
+    def measurement_function(self, **kwargs):
         """defines what to call to measure"""
-        results = self.get_data()
-        no_pi_real = results[0][:, 0]
-        no_pi_im = results[1][:, 0]
-        pi_real = results[0][:, 1]
-        pi_im = results[1][:, 1]
+        if self.from_saved_data:
+            max_separation = self.get_data(self.runid, self.measured_params.keys(), **kwargs)[0]
+        else:
+            results = self.get_data()
+            no_pi_real = results[0][:, 0]
+            no_pi_im = results[1][:, 0]
+            pi_real = results[0][:, 1]
+            pi_im = results[1][:, 1]
 
-        fidelity_info = calculate_fidelity(no_pi_real, no_pi_im, pi_real, pi_im)
+            fidelity_info = calculate_fidelity(no_pi_real, no_pi_im, pi_real, pi_im)
+            max_separation = fidelity_info.max_separation
 
-        return [fidelity_info.max_separation]
+        return [max_separation]
 
     def stopping_condition(self, num_attempts):
         if num_attempts >= self.max_attempts:
@@ -53,21 +59,18 @@ class ReadoutFidelityOptimization:
 
     def cost_val(self, measurement):
         if isinstance(measurement, dict):
-            val = measurement['readout_fidelity']
-        elif isinstance(measurement, float):
-            val = measurement
-        elif isinstance(measurement, list):
-            if len(measurement) > 1:
-                raise RuntimeError(f"Too many values for measurement - expected 1 and got {len(measurement)}")
-            val = measurement[0]
-        else:
-            raise RuntimeError(f"I don't know what to do with a measurement in this format: {measurement}")
+            measurement = [measurement[key] for key in self.measured_params]
+        if len(measurement) != 1:
+            raise RuntimeError(f"Expected 1 value from measurement and got {len(measurement)}")
+
+        val = float(measurement[0])
+
         return 1-val
 
 
 class Rabis:
 
-    def __init__(self, *params, get_data, max_attempts=250):
+    def __init__(self, *params, get_data, max_attempts=20):
         # get_data = pwa.alazar_channels.data
         # ToDo: figure out better solution for step size?
         self.params = [item for item in params if isinstance(item, qc.Parameter)]
@@ -99,10 +102,10 @@ class Rabis:
         fit = fitter.fit(data, x=pulse_dur)
 
         if fit[0] is None:
-            pi_pulse_duration=None
+            pi_pulse_duration = None
         else:
             omega = fit[0]['w']
-            pi_pulse_duration= np.pi / omega
+            pi_pulse_duration = np.pi / omega
         return [pi_pulse_duration]
 
     def stopping_condition(self, num_attempts):
@@ -113,20 +116,15 @@ class Rabis:
 
     def cost_val(self, measurement):
         if isinstance(measurement, dict):
-            val = measurement['pi_pulse_duration']
-        elif isinstance(measurement, float):
-            val = measurement
-        elif isinstance(measurement, list):
-            if len(measurement) > 1:
-                raise RuntimeError(f"Too many values for measurement - expected 1 value and got {len(measurement)}")
-            val = measurement[0]
-        else:
-            raise RuntimeError(f"I don't know what to do with a measurement in this format: {measurement}")
+            measurement = [measurement[key] for key in self.measured_params]
+        if len(measurement) != 1:
+            raise RuntimeError(f"Expected 1 value from measurement and got {len(measurement)}")
+
+        val = measurement[0]
 
         if val is None:
             return 1e15
         else:
-            #Todo: it would be better if this could deal with having BOTH parameters, instead of only 1
             if len(self.params) > 1:
                 raise RuntimeError(f"Trying to optimize {self.params}, but you haven't set this one up to do multiple parameters at once!")
             param = self.params[0]
@@ -137,7 +135,8 @@ class Rabis:
             elif param.name == 'power':
                 return np.abs(50e-9-val)
             else:
-                raise RuntimeError(f"I don't know how to optimize {param.full_name} based on pi pulse duration")
+                raise RuntimeError(f"I don't know how to optimize {param.full_name} based on pi pulse duration, "
+                                   f"because it is not included in the cost_val function")
 
 
 class BestNeighbour(ReadoutFidelityOptimization):
@@ -152,20 +151,33 @@ class BestNeighbour(ReadoutFidelityOptimization):
         for i, param in enumerate(self.params):
             # select randomly to go anywhere from 1-10 steps
             step = randint(1, 11) * self.step_size[i]
-            next_param_vals[param.full_name] = [current_location[param.full_name] - step,
-                                                current_location[param.full_name] + step]
+            if self.from_saved_data:
+                param_name = param
+            else:
+                param_name = param.full_name
+            next_param_vals[param_name] = [current_location[param_name] - step,
+                                           current_location[param_name] + step]
 
         param_combinations = product(*[set(v) for v in next_param_vals.values()])
 
         for combination in param_combinations:
-            d = {}
-            for i, param in enumerate(self.params):
-                param(combination[i])
-                d[param.full_name] = combination[i]
-            measurement = self.measurement_function()
-            for i, measured_param in enumerate(self.measured_params):
-                d[measured_param] = measurement[i]
-            next_locations.append(d)
+            if self.from_saved_data:
+                d = {}
+                for i, param in enumerate(self.params):
+                    d[param] = combination[i]
+                measurement = self.measurement_function(**d)
+                for i, measured_param in enumerate(self.measured_params):
+                    d[measured_param] = measurement[i]
+                next_locations.append(d)
+            else:
+                d = {}
+                for i, param in enumerate(self.params):
+                    param(combination[i])
+                    d[param.full_name] = combination[i]
+                measurement = self.measurement_function()
+                for i, measured_param in enumerate(self.measured_params):
+                    d[measured_param] = measurement[i]
+                next_locations.append(d)
 
         return next_locations
 
--- qdev_wrappers/optimization/Optimization.py
+++ qdev_wrappers/optimization/Optimization.py
@@ -1,41 +1,48 @@
 from qcodes.dataset.measurements import Measurement
 import numpy as np
-from optimization_from_completed_data import get_measurement_from_data
-from Methods import ReadoutFidelityOptimization
-
+from qdev_wrappers.optimization.optimization_from_completed_data import get_measured_data
 
 class Optimization:
 
-    #ToDo: separate optimize function from Optimization, so that it creates and returns an optimization instead
-    #ToDo: params and step size in optimization instead of in method
-    #ToDo: make measurement optional when optimizing
+    # ToDo: separate optimize function from Optimization, so that it creates and returns an optimization instead
+    # ToDo: params and step size in optimization instead of in method
+    # ToDo: make measurement optional when optimizing?
 
-    def __init__(self, method: ReadoutFidelityOptimization):
+    def __init__(self, method, start=None):
         self.method = method
         self.params = method.params
         self.num_attempts = 0
-        self.start = {param.full_name: param() for param in self.params}
+
+        if method.from_saved_data:
+            self.start = start
+        else:
+            self.start = {param.full_name: param() for param in self.params}
 
         self.current = self.start.copy()
-        initial_measurement = method.measurement_function()
+        initial_measurement = method.measurement_function(**start)
         for i, param in enumerate(method.measured_params):
             self.current[param] = initial_measurement[i]
 
-        self.best_cost_val = method.cost_val(method.measurement_function())
-        self.best = self.start
+        self.best_cost_val = method.cost_val(self.current)
+        self.best = self.current.copy()
 
     def optimize(self, *params):
 
-        self.method.params = [param for param in params]
-        #ToDo: this is not great - it needs to update step size too, and these things shouldn't be stored in the model.
-        #But maybe a list of possible parameters and their default step sizes are stored in the model?
+        # ToDo: this is not great - it needs to update step size too, and these things shouldn't be stored in the model.
+        # But maybe a list of possible parameters and their default step sizes are stored in the model?
 
         meas = Measurement()
 
         setpoints = []
-        for parameter in self.method.params:
-            setpoints.append(parameter.full_name)
-            meas.register_parameter(parameter)
+        if self.method.from_saved_data:
+            for parameter in self.method.params:
+                setpoints.append(parameter)
+                meas.register_custom_parameter(name=parameter,
+                                               label=parameter)
+        else:
+            for parameter in self.method.params:
+                setpoints.append(parameter.full_name)
+                meas.register_parameter(parameter)
         for parameter, param_info in self.method.measured_params.items():
             meas.register_custom_parameter(name=parameter,
                                            label=param_info['label'],
@@ -44,6 +51,9 @@ class Optimization:
 
         with meas.run() as datasaver:
             run_id = datasaver.run_id
+
+            res = [(param, val) for param, val in self.current.items()]
+            datasaver.add_result(*res)
 
             while not self.method.stopping_condition(self.num_attempts):
                 # check new parameter settings, select one to move to
@@ -72,39 +82,39 @@ class Optimization:
 
         print(f"Best: {self.best}")
         return run_id
-
-    def try_many(self, *params, num_repetitions=100, success_condition=None):
-        # ToDo: change this to be for optimization while measuring
-        # ToDo: make into measurement that records number of iterations, time for optimization, optimal parameters
-        # success_condition: a function that takes the best_value and returns True for a success, otherwise False
-        if success_condition is not None:
-            successes = 0
-        measurements_done = 0
-        iterations = 0
-        best_value = []
-        best_coordinates = []
-        starts = []
-
-        for attempt in range(0, num_repetitions):
-            start, checked, best = self.optimize(*params)
-
-            iterations += len(checked)
-            measurements_done += len(np.unique(checked))
-            starts.append(start)
-            best_coordinates.append(best)
-            best_value.append(get_measurement_from_data(params, self.method.measured_params, best))
-
-            if success_condition is not None:
-                success = success_condition(best_value)
-                if success:
-                    successes += 1
-
-        avg_measurements = measurements_done / num_repetitions
-        avg_num_iterations = iterations / num_repetitions
-
-        if num_repetitions % 10 == 0:
-            print(num_repetitions)
-        if success_condition is not None:
-            return best_coordinates, best_value, starts, avg_measurements, avg_num_iterations, successes/num_repetitions
-        else:
-            return best_coordinates, best_value, starts, avg_measurements, avg_num_iterations
+    #
+    # def try_many(self, *params, num_repetitions=100, success_condition=None):
+    #     # ToDo: change this to be for optimization while measuring
+    #     # ToDo: make into measurement that records number of iterations, time for optimization, optimal parameters
+    #     # success_condition: a function that takes the best_value and returns True for a success, otherwise False
+    #     if success_condition is not None:
+    #         successes = 0
+    #     measurements_done = 0
+    #     iterations = 0
+    #     best_value = []
+    #     best_coordinates = []
+    #     starts = []
+    #
+    #     for attempt in range(0, num_repetitions):
+    #         start, checked, best = self.optimize(*params)
+    #
+    #         iterations += len(checked)
+    #         measurements_done += len(np.unique(checked))
+    #         starts.append(start)
+    #         best_coordinates.append(best)
+    #         best_value.append(get_measured_data(params, self.method.measured_params, best))
+    #
+    #         if success_condition is not None:
+    #             success = success_condition(best_value)
+    #             if success:
+    #                 successes += 1
+    #
+    #     avg_measurements = measurements_done / num_repetitions
+    #     avg_num_iterations = iterations / num_repetitions
+    #
+    #     if num_repetitions % 10 == 0:
+    #         print(num_repetitions)
+    #     if success_condition is not None:
+    #         return best_coordinates, best_value, starts, avg_measurements, avg_num_iterations, successes/num_repetitions
+    #     else:
+    #         return best_coordinates, best_value, starts, avg_measurements, avg_num_iterations
